96986c5f1c4150ab1c0061e759351083d3531dea
mexcaptain <mikhail@hashboard.ru>
2017-06-14 17:22:16 +0300

    Fix 1

--- 5/1.js
+++ 5/1.js
@@ -1,75 +1,73 @@
-const express = require("express");
+function getMaxId(list) {
+	const arr = list.map(p => p.id);
+	if (!arr.length) return 0;
+	return Math.max.apply(null, arr);
+}
+
+const express = require('express');
 const app = express();
 
-var bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 let users = [
-	{"id":1,"name":"Mikhail","score":100},
-	{"id":2,"name":"Ivan","score":50}
-]
+	{id:1, name:'Mikhail', score:100},
+	{id:2, name:'Ivan', score:50}
+];
 
 const rtAPIv1 = express.Router();
 
 // Get all users.
-rtAPIv1.get("/users/", function(req, res) {
+rtAPIv1.get('/users/', function(req, res) {
 	if (users.length > 0) {
-		res.send(users);
-	}
-	else {
-		res.send('Users is empty')
+		res.status(200).send(users);
+	} else {
+		res.status(200).send('Users is empty')
 	}
 });
 
 // Get user by id.
-rtAPIv1.get("/users/:id", function(req, res) {
+rtAPIv1.get('/users/:id', function(req, res) {
 	let ans = users.filter(function(user) {
-		return (user.id == req.params.id);
+		return (user.id === req.params.id);
 	});
 	if (ans.length > 0) {
-		res.send(ans)
-	}
-	else {
-		res.send('User (id: ' + req.params.id + ') not found')
+		res.status(200).send(ans);
+	} else {
+		res.status(404).send(`User (id: ${req.params.id}) not found`);
 	}
 });
 
 // Add user
-rtAPIv1.post("/users/", function(req, res) {
+rtAPIv1.post('/users/', function(req, res) {
 	let name = req.body.name;
 	let score = req.body.score;
-	let id = 0;
-	for (var i=0 ; i<users.length ; i++) {
-		if (users[i].id > id) {
-			id = users[i].id
-		}
-	}
-	id++;
+	let id = getMaxId(users) + 1;
 	users.push({'id':id, 'name':name, 'score':score});
-	res.send('added ' + name);
+	res.status(200).send('added ' + name);
 });
 
 // Edit user by id
-rtAPIv1.put("/users/:id", function(req, res) {
+rtAPIv1.put('/users/:id', function(req, res) {
 	let editUser = false;
 	for (let i=0; i<users.length; i++) {
 		if (users[i].id == req.params.id) {
 			editUser = true;
-			if (req.body.name && req.body.name != '') {
+			if (req.body.name) {
 				users[i].name = req.body.name;
 			}
-			if (req.body.score && req.body.score != '') {
+			if (req.body.score) {
 				users[i].score = req.body.score;
 			}
 			break;
 		}
 	}
 	if (editUser) {
-		res.send(users[req.params.id]);
+		res.status(200).send(users[req.params.id]);
 	}
 	else {
-		res.send('User (id: ' + req.params.id + ') not found')
+		res.status(404).send(`User (id: ${req.params.id}) not found`);
 	}
 });
 
@@ -84,18 +82,18 @@ rtAPIv1.delete('/users/:id', function(req, res) {
 		}
 	}
 	if (deleteUser) {
-		res.end('Deleted');
+		res.status(200).send('Deleted');
 	}
 	else {
-		res.send('User (id: ' + req.params.id + ') not found')
+		res.status(404).send(`User (id: ${req.params.id}) not found`);
 	}
 });
 
-app.get("/", function(req, res) {
-	res.send("Hello World1!");
+rtAPIv1.get('/', function(req, res) {
+	res.status(200).send('Hello World!');
 });
 
-app.use("/api/v1", rtAPIv1);
+app.use('/api/v1', rtAPIv1);
 
 
 app.listen(1337);
