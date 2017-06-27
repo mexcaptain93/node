2e97e00a58da8b3f263535da2cb6bded5b9367d1
mexcaptain <mikhail@hashboard.ru>
2017-06-27 16:44:37 +0300

    Fix

--- 4/server.js
+++ 4/server.js
@@ -26,6 +26,7 @@ function getHash(data, res) {
 		});
 		response.on('end', () => {
 			const answer = process(remoteData);
+			res.writeHead(200, 'OK', {'Content-Type': 'text/plain'});
 			res.write(JSON.stringify(answer));
 			res.end();
 		});
@@ -42,7 +43,6 @@ function handler(req, res) {
 	let data = '';
 	req.on('data', chunk => data += chunk);
 	req.on('end', () => {
-		res.writeHead(200, 'OK', {'Content-Type': 'text/plain'});
 		getHash(data, res);
 	});
 }
