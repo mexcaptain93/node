87612956cdb9d17eb80383ca369314aa671be336
mexcaptain <mikhail@hashboard.ru>
2017-07-11 11:50:18 +0300

    Fix

--- angular4/app/src/BerriesService/BerriesService.js
+++ angular4/app/src/BerriesService/BerriesService.js
@@ -1,9 +1,7 @@
 angular
 	.module('PokemonApp')
 	.factory('BerriesService', function($resource) {
-		return $resource('https://api.backendless.com/80153656-A0C1-1735-FFB1-1A7318567300/B68E5193-B06A-923C-FF8B-0C188BA7A000/data/berries', {
-			pokemonId: '@pokemonId'
-		}, {
+		return $resource('https://api.backendless.com/80153656-A0C1-1735-FFB1-1A7318567300/B68E5193-B06A-923C-FF8B-0C188BA7A000/data/berries', {}, {
 			query: {
 				method: 'GET',
 				headers: { 'application-id': undefined, 'secret-key': undefined },
